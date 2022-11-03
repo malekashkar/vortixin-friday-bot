f864221ce3960bd16eea70369080cc19ee6275ef
malekashkar <ashkarmalek0@gmail.com>
2022-11-03 08:05:43 -0400

    Covered MongoDB URL

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://user:5A3aDlTy@cluster0.lmlfo.mongodb.net/database`, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
